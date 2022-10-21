89291dcd0b4de26acbbf2cf858b146eab95526a0
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-20 21:16:10 -0300

    menuBurguer

---
+++ exercicios/ex026/mq005/scripts/interface.js
@@ -0,0 +1,13 @@
+
+
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+        burguer.style.display = 'block'
+        burguerOn.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+        burguer.style.display = 'none'
+        burguerOn.style.display = 'block'
+    }
+}
