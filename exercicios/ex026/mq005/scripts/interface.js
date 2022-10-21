9c5bb26b4884d4ddf94a2543bdd71cae4c3950e5
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-10-20 21:44:48 -0300

    up-last26

--- exercicios/ex026/mq005/scripts/interface.js
+++ exercicios/ex026/mq005/scripts/interface.js
@@ -3,11 +3,19 @@
 function clickMenu() {
     if (itens.style.display == 'block') {
         itens.style.display = 'none'
-        burguer.style.display = 'block'
-        burguerOn.style.display = 'none'
+        // burguer.style.display = 'block'
+        // burguerOn.style.display = 'none'
     } else {
         itens.style.display = 'block'
-        burguer.style.display = 'none'
-        burguerOn.style.display = 'block'
+        // burguer.style.display = 'none'
+        // burguerOn.style.display = 'block'
+    }
+}
+
+function mudarTamanho() {
+    if (window.innerWidth >= 768) {
+        itens.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
     }
 }
